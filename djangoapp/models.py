5c0f69a2592084277481f2f0b75b03c147002e92
Vitalii Bryl <bryl@zp.edu.ua>
2022-11-13 21:39:50 +0200

    init

---
+++ djangoapp/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class Measurement(models.Model):
+    value = models.CharField(max_length=50)
+    description = models.CharField(max_length=250, blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.value 
+
+#mr = Measurement()
+#mr.value = 'aertw'
+#mr.description = 'do not buy lanos'
+#mr.save()
---
+++ djangoapp/views.py
@@ -0,0 +1,21 @@
+#from django.shortcuts import render
+import os
+from django.views import generic
+from django.conf import settings
+from djangoapp.models import Measurement
+
+class MeasurementList (generic.ListView):
+    model = Measurement
+    context_object_name = 'measurements'
+    template_name = 'measurement_list.html'
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            file_path = os.path.join(settings.BASE_DIR, 'test_data')
+            measurement_file = open(file_path, 'r')
+            
+            for value in measurement_file:
+                Measurement.object.get_or_create(value=value)
+        except IOError:
+                pass
+        return super(MeasurementList, self).get(request, *args, **kwargs)
