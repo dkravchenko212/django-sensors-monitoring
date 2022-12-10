2d57d692c3b24f14485ed77906914725a91f070e
dkravchenko212 <dkravchenko@zp.edu.ua>
2022-12-10 03:19:28 +0200

    added saving measurements to mongo

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Measurement(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     value = models.CharField(max_length=50)
+    sensor_name = models.CharField(max_length=100, blank=False, null=False)
     description = models.CharField(max_length=250, blank=True, null=True)
     
     def __unicode__(self):
--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -1,13 +1,19 @@
 from djangoapp.services.sensors_service import SensorsService, SwitchState
+from djangoapp.models import Measurement
 import time
-
-def cron_test():
-    print("It works!")
-    
+   
 def update_data_from_sensors():
     sensors = SensorsService()
     res = sensors.get_data_from_dht11()
     print("Data from DHT11 : {}".format(res))
+    if res[0] != 0:
+        print("Saving measurement from DHT11")
+        mr = Measurement()
+        mr.sensor_name = "DHT11"
+        mr.value = "Temperature: {} Humidity: {}".format(res[0], res[0])
+        mr.description = "sensor connected to orangepi 3 lts"
+        mr.save()
+        print("Done saving")
     print("Switching relay state OFF")
     sensors.switch_relay(state=SwitchState.OFF)
     time.sleep(1)
